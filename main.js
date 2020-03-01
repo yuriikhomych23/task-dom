cb43aab7a539269c88a42a1d120b1c6a8f32ccd6
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-03-01 14:53:43 +0000

    Sample code

---
+++ main.js
@@ -0,0 +1,10 @@
+let background = prompt('Write background');
+let print = prompt('write a print');
+let title = prompt('write title');
+let backgroundP = prompt('write background paragraph');
+let textColor = prompt('write text color');
+let all = prompt('write height length color');
+let marker = prompt('write marker');
+
+
+let 
